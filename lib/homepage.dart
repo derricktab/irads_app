14b7e40b96d83b1e9f59ec9d9593d9e728484852
DERRICK ZZIWA <derricktab44@gmail.com>
2022-12-27 06:46:00 +0300

    login form

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -8,6 +8,11 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  var _key = GlobalKey<FormState>();
+  var email = TextEditingController();
+  var password = TextEditingController();
+  FocusNode focusNode = FocusNode();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,35 +21,80 @@ class _HomepageState extends State<Homepage> {
         child: Row(
           children: [
             Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text("LOGIN"),
+                // LOGIN TEXT
+                const Text(
+                  "LOGIN",
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "times",
+                  ),
+                ),
+
+                const SizedBox(height: 50),
+
+                // LOGIN FORM WRAPPER
                 Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 70),
+                  padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 30),
+                  width: MediaQuery.of(context).size.width * 0.45,
                   decoration: BoxDecoration(
                     color: Colors.blue,
                     borderRadius: BorderRadius.circular(20),
                   ),
+
+                  // LOGIN FORM
                   child: Form(
+                      key: _key,
                       child: Column(
-                    children: [
+                        children: [
+                          // EMAIL FORM FIELD
+                          TextFormField(
+                            decoration:
+                                InputDecoration(fillColor: Colors.white),
+                            controller: email,
+                            focusNode: focusNode,
+                            keyboardType: TextInputType.emailAddress,
+                            validator: (value) {
+                              if (value == null) {
+                                return "The Email Field Is required";
+                              }
+                              return null;
+                            },
+                          ),
 
-                      // USERNAME FORM FIELD
-                      TextFormField(),
+                          const SizedBox(height: 15),
 
-                      // PASSWORD FIELD
-                      TextFormField(),
+                          // PASSWORD FIELD
+                          TextFormField(
+                            controller: password,
+                            focusNode: focusNode,
+                            obscureText: true,
+                            validator: (value) {
+                              if (value == null) {
+                                return "The Password Field Cant Be Empty";
+                              }
+                              return null;
+                            },
+                          ),
 
-                      // LOGIN BUTTON
-                      ElevatedButton(
-                        onPressed: (() {
-                          print("LOGIN");
-                        }),
-                        child: Text("LOGIN"),
-                      ),
-                    ],
-                  )),
+                          const SizedBox(height: 15),
+
+                          // LOGIN BUTTON
+                          ElevatedButton(
+                            onPressed: (() {
+                              print("LOGIN");
+                            }),
+                            child: const Text("LOGIN"),
+                          ),
+                        ],
+                      )),
                 )
               ],
-            )
+            ),
+            Image.asset("assets/images/accident.png"),
           ],
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:irads_app/firebase_options.dart';
 import 'package:irads_app/homepage.dart';
 
-void main() {
+void main() async {
   runApp(const MyApp());
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
 }
 
 class MyApp extends StatelessWidget {
